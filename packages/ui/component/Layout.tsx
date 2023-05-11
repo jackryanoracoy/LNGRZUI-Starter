ff3d720b8d0a7e703eaa21b5ff9a9868778c5b0b
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2023-05-12 00:25:13 +0800

    Made some changes to the layout component.

--- packages/ui/component/Layout.tsx
+++ packages/ui/component/Layout.tsx
@@ -4,21 +4,29 @@ import '../styles/Global.scss';
 import '../styles/Layout.scss';
 
 export const Layout: React.FC<CommonProps> = ({ children }) => {
-  return <>{children}</>;
+  const hasChildLayout = React.Children.toArray(children).some(
+    child => React.isValidElement(child) && child.type === Layout
+  );
+  return hasChildLayout ? <div className='layout-container'>{children}</div> : <div className='layout'>{children}</div>;
 };
 
 export const Header: React.FC<CommonProps> = ({ children, className = '' }) => {
-  const classNames = className ? `header ${className}` : `header`;
+  const classNames = className ? `header ${className}` : 'header';
   return <header className={classNames}>{children}</header>;
 };
 
 export const Main: React.FC<CommonProps> = ({ children, className = '' }) => {
-  const classNames = className ? `main ${className}` : `main`;
+  const classNames = className ? `main ${className}` : 'main';
   return <main className={classNames}>{children}</main>;
 };
 
+export const Sider: React.FC<CommonProps> = ({ children, className = '' }) => {
+  const classNames = className ? `aside ${className}` : 'aside';
+  return <aside className={classNames}>{children}</aside>;
+};
+
 export const Footer: React.FC<CommonProps> = ({ children, className = '' }) => {
-  const classNames = className ? `footer ${className}` : `footer`;
+  const classNames = className ? `footer ${className}` : 'footer';
   return <footer className={classNames}>{children}</footer>;
 };
 
