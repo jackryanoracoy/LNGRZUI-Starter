85c1f1f2090ea8cea8c772aa7b411158024201d0
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2023-06-05 01:13:20 +0800

    Made some fixes and changes...

--- apps/docs/component/PageLayout.tsx
+++ apps/docs/component/PageLayout.tsx
@@ -1,29 +1,14 @@
-import { useRouter } from 'next/router';
 import { CommonProps } from './ComponentTypes';
 import { Layout, Header, Main, Sider, Footer } from 'ui';
 import PageHeader from '../component/PageHeader';
-import PageSider from '../component/PageSider';
 import PageFooter from '../component/PageFooter';
 
 export default function PageLayout({ children }: CommonProps) {
-  const router = useRouter();
-  const home = router.pathname === '/';
   return (
-    home ? (
-      <Layout>
-        <Header><PageHeader /></Header>
-        <Main>{children}</Main>
-        <Footer><PageFooter /></Footer>
-      </Layout>
-    ) : (
-      <Layout>
-        <Header><PageHeader /></Header>
-        <Layout>
-          <Sider><PageSider /></Sider>
-          <Main>{children}</Main>
-        </Layout>
-        <Footer><PageFooter /></Footer>
-      </Layout>
-    )
+    <Layout>
+      <Header><PageHeader /></Header>
+      <Main>{children}</Main>
+      <Footer><PageFooter /></Footer>
+    </Layout>
   )
 }
